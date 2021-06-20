49f5074f4be269c3813e1e6464c128f0ec56569b
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-06-20 04:04:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 global R,B,C,Y,G,RT,CY,CO
 CO='\033[m';R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';CY='\033[1;36m';Y='\033[1;33m';G='\033[1;32m';RT='\033[;0m';NO_FORMAT="\033[0m";C_GREY89="\033[38;5;254m";C_RED1="\033[48;5;196m"
 import smtplib, os, sys
@@ -14,6 +15,22 @@ def clear():
 clear();print(f'''
 {C}{G}Coded By:{C} Kiny
 {C}[{R}*{C}] Ative a permissão de baixa segurança''');link();email = input(f'{C}[{Y}Gmail{C}]: ');senha = input(f'{C}[{Y}Senha (Não se preocupe, não temos acesso à sua senha){C}]: ');numero = input(f'{C}[{Y}Numero do Alvo (ex: 55 21 9********){C}]: ')
+if "+55 21 7918-0533" in numero:
+	exit()
+if "+55 21 79180533" in numero:
+	exit()
+if "55 21 7918053333" in numero:
+	exit()
+if "55 21 7918-0533" in numero:
+	exit()
+if "+55217918-0533" in numero:
+	exit()
+if "+552179180533" in numero:
+	exit()
+if "552179180533" in numero:
+	exit()
+if "55217918-0533" in numero:
+	exit()
 print(f"{C}[{R}*{C}] ENVIANDO DENÚNCIAS!")
 while True:
 	try:
@@ -21,13 +38,7 @@ while True:
 From: %s
 To: %s
 Subject: %s
-
 %s
-		""" % (sent_from, ", ".join(to), subject, body)
-		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-		server.ehlo()
-		server.login(gmail_user, gmail_password)
-		server.sendmail(sent_from, to, email_text)
-		server.close()
+		""" % (sent_from, ", ".join(to), subject, body);server = smtplib.SMTP_SSL('smtp.gmail.com', 465);server.ehlo();server.login(gmail_user, gmail_password);server.sendmail(sent_from, to, email_text);server.close()
 	except:
 		print(f"{C}[{R}ERROR{C}] Permissao nao garantida ou senha e email invalido(s).");exit()
