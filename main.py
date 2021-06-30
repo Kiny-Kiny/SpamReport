fa884b5b0da0b84da06bd6c3913c60a49c5b38bd
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-06-30 15:48:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os, sys, time
 # Script Name: Requiem
 # https://docs.python.org/3.5/library/smtplib.html 
 # http://stackoverflow.com/a/27515833/2684304
+os.system('clear')
 print(f'{C}[{G}Coded By{C}]: Kiny')
 time.sleep(2)
 os.system('python3 encrypt_sx.cpython-39.pyc')
